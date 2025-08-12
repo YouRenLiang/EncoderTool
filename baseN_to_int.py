0241e2b6c54877b4a040b496527a61fb7fa2291b
ren.liang (梁佑任) <ren.liang@tjci.org.tw>
2025-08-12 12:00:39 +0800

    新增flask代理

--- baseN_to_int.py
+++ baseN_to_int.py
@@ -4,7 +4,6 @@ def baseN_to_int(text, base_chars):
     char_to_val = {c: i for i, c in enumerate(base_chars)}
     num = 0
     for c in text:
-        if c not in char_to_val:
-            raise ValueError(f"Invalid character '{c}' not in base chars.")
-        num = num * base + char_to_val[c]
+        if c in char_to_val:
+            num = num * base + char_to_val[c]
     return num
